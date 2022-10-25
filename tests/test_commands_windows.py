5e24b066293b1fc622c7aa8dc1fedb0b8e4ac621
ahuang11 <andrew.h@prefect.io>
2022-10-24 17:04:49 -0700

    Add default shell

--- tests/test_commands_windows.py
+++ tests/test_commands_windows.py
@@ -3,14 +3,17 @@ import logging
 import os
 import sys
 from pathlib import Path
+from unittest.mock import AsyncMock
 
 import pytest
 from prefect import flow
 
 from prefect_shell.commands import shell_run_command
 
+if sys.platform != "win32":
+    pytest.skip(reason="see test_commands.py", allow_module_level=True)
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
+
 def test_shell_run_command_error_windows(prefect_task_runs_caplog):
     @flow
     def test_flow():
@@ -22,7 +25,6 @@ def test_shell_run_command_error_windows(prefect_task_runs_caplog):
     assert len(prefect_task_runs_caplog.records) == 0
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_windows(prefect_task_runs_caplog):
     prefect_task_runs_caplog.set_level(logging.INFO)
     echo_msg = "_THIS_ IS WORKING!!!!"
@@ -44,7 +46,6 @@ def test_shell_run_command_windows(prefect_task_runs_caplog):
         raise AssertionError
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_stream_level_windows(prefect_task_runs_caplog):
     prefect_task_runs_caplog.set_level(logging.WARNING)
     echo_msg = "_THIS_ IS WORKING!!!!"
@@ -69,7 +70,6 @@ def test_shell_run_command_stream_level_windows(prefect_task_runs_caplog):
         raise AssertionError
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_helper_command_windows():
     @flow
     def test_flow():
@@ -82,7 +82,6 @@ def test_shell_run_command_helper_command_windows():
     assert test_flow() == os.path.expandvars("$USERPROFILE")
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_cwd():
     @flow
     def test_flow():
@@ -95,7 +94,6 @@ def test_shell_run_command_cwd():
     assert test_flow() == os.fspath(Path.home())
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_return_all():
     @flow
     def test_flow():
@@ -108,7 +106,6 @@ def test_shell_run_command_return_all():
     assert result[1].rstrip() == "yes!"
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_no_output_windows():
     @flow
     def test_flow():
@@ -117,7 +114,6 @@ def test_shell_run_command_no_output_windows():
     assert test_flow() == ""
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_uses_current_env_windows():
     @flow
     def test_flow():
@@ -129,7 +125,6 @@ def test_shell_run_command_uses_current_env_windows():
     assert result[0].rstrip() == os.environ["USERPROFILE"]
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_update_current_env_windows():
     @flow
     def test_flow():
@@ -146,7 +141,6 @@ def test_shell_run_command_update_current_env_windows():
     assert result[1] == "test value"
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_ensure_suffix_ps1():
     @flow
     def test_flow():
@@ -156,7 +150,6 @@ def test_shell_run_command_ensure_suffix_ps1():
     assert result == "2"
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="see test_commands.py")
 def test_shell_run_command_ensure_tmp_file_removed():
     @flow
     def test_flow():
@@ -167,3 +160,35 @@ def test_shell_run_command_ensure_tmp_file_removed():
     test_flow()
     temp_dir = os.environ["TEMP"]
     assert len(glob.glob(f"{temp_dir}\\prefect-*.ps1")) == 0
+
+
+class AsyncIter:
+    def __init__(self, items):
+        self.items = items
+
+    async def __aiter__(self):
+        for item in self.items:
+            yield item
+
+
+@pytest.mark.parametrize("shell", [None, "powershell", "powershell.exe"])
+def test_shell_run_command_override_shell(shell, monkeypatch):
+    open_process_mock = AsyncMock()
+    stdout_mock = AsyncMock()
+    stdout_mock.receive.side_effect = lambda: b"received"
+    open_process_mock.return_value.__aenter__.return_value = AsyncMock(
+        stdout=stdout_mock
+    )
+    open_process_mock.return_value.__aenter__.return_value.returncode = 0
+    monkeypatch.setattr("prefect_shell.commands.open_process", open_process_mock)
+    monkeypatch.setattr("prefect_shell.commands.TextReceiveStream", AsyncIter)
+
+    @flow
+    def test_flow():
+        return shell_run_command(
+            command="echo 'testing'",
+            shell=shell,
+        )
+
+    test_flow()
+    assert open_process_mock.call_args_list[0][0][0][0] == shell or "powershell"
