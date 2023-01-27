5549f12dc7239f438cb0fff3cf166bceb96348f3
ahuang11 <andrew.h@prefect.io>
2023-01-27 11:56:15 -0800

    Add context management

--- prefect_shell/__init__.py
+++ prefect_shell/__init__.py
@@ -1,4 +1,4 @@
 from . import _version
-from .commands import shell_run_command, ShellJob  # noqa
+from .commands import shell_run_command, ShellCommand  # noqa
 
 __version__ = _version.get_versions()["version"]
--- prefect_shell/commands.py
+++ prefect_shell/commands.py
@@ -8,19 +8,17 @@ import subprocess
 import sys
 import tempfile
 from contextlib import AsyncExitStack
-from typing import Any, Dict, List, Optional, TextIO, Union
+from typing import Any, Dict, List, Optional, Union
 
 import anyio
 from anyio.abc import Process
-from anyio.streams.text import TextReceiveStream, TextSendStream
+from anyio.streams.text import TextReceiveStream
 from prefect import task
 from prefect.blocks.abstract import JobBlock, JobRun
 from prefect.logging import get_run_logger
 from prefect.utilities.asyncutils import sync_compatible
 from prefect.utilities.processutils import open_process
 from pydantic import Field, PrivateAttr
-
-TextSink = Union[anyio.AsyncFile, TextIO, TextSendStream]
 
 
 @task
@@ -123,29 +121,49 @@ async def shell_run_command(
     return lines if return_all else line
 
 
-class ShellJobRun(JobRun):
+class ShellProcess(JobRun):
     """
     A class representing a run of a shell job.
     """
 
-    def __init__(self, shell_job: "ShellJob", process: Process):
-        self._shell_job = shell_job
+    def __init__(self, shell_operation: "ShellCommand", process: Process):
+        self._shell_operation = shell_operation
         self._process = process
         self._output = []
+
+    @property
+    def pid(self) -> int:
+        """
+        The PID of the process.
+
+        Returns:
+            The PID of the process.
+        """
+        return self._process.pid
+
+    @property
+    def return_code(self) -> Optional[int]:
+        """
+        The return code of the process.
+
+        Returns:
+            The return code of the process, or `None` if the process is still running.
+        """
+        return self._process.returncode
 
     async def _capture_output(self, source):
         """
         Capture output from source.
         """
         async for text in TextReceiveStream(source):
-            if self._shell_job.stream_output:
+            if self._shell_operation.stream_output:
                 self.logger.info(text)
             self._output.append(text)
 
     @sync_compatible
     async def wait_for_completion(self):
         """
-        Wait for the job run to complete.
+        Wait for the shell operation to complete.
         """
         await asyncio.gather(
             self._capture_output(self._process.stdout),
@@ -155,19 +173,35 @@ class ShellJobRun(JobRun):
     @sync_compatible
     async def fetch_result(self) -> List[str]:
         """
-        Retrieve the results of the job run and return them.
+        Retrieve the results of the shell operation and return them.
+
+        Returns:
+            The lines output from the shell operation as a list.
         """
         return self._output
 
 
-class ShellJob(JobBlock):
+class ShellCommand(JobBlock):
     """
-    A block representing a shell job.
+    A block representing a shell command.
+
+    It is recommended to use this block as a context manager, which will automatically
+    close all of its opened processes when the context is exited. If not, the user
+    should call `close` to ensure all processes are closed.
 
     Attributes:
         command: The command to run.
         stream_output: Whether to stream output.
         env: Environment variables to use for the subprocess.
+        working_dir: The working directory context the command will be executed within.
+
+    Examples:
+        Load a configured block:
+        ```python
+        from prefect_shell import ShellCommand
+
+        shell_command = ShellCommand.load("BLOCK_NAME")
+        ```
     """
 
     command: str = Field(default=..., description="The command to run.")
@@ -177,19 +211,31 @@ class ShellJob(JobBlock):
         title="Environment Variables",
         description="Environment variables to use for the subprocess.",
     )
+    working_dir: str = Field(
+        default=None,
+        title="Working Directory",
+        description=(
+            "The working directory context the command will be executed within."
+        ),
+    )
 
     _exit_stack: AsyncExitStack = PrivateAttr(
         default_factory=AsyncExitStack,
     )
 
     @sync_compatible
-    async def trigger(self, **open_kwargs: Dict[str, Any]) -> ShellJobRun:
+    async def trigger(self, **open_kwargs: Dict[str, Any]) -> ShellProcess:
         """
-        Triggers a job run in an external service and returns a JobRun object
-        to track the execution of the run.
+        Triggers a shell operation and returns the shell operation run object
+        to track the execution of the run. This method is ideal for long-lasting
+        shell operations; for short-lasting shell operations, it is recommended
+        to use `run` instead.
 
         Args:
             **open_kwargs: Additional keyword arguments to pass to `open_process`
+
+        Returns:
+            A `ShellProcess` object.
         """
         command_args = shlex.split(self.command)
 
@@ -202,14 +248,63 @@ class ShellJob(JobBlock):
                 stdout=subprocess.PIPE if self.stream_output else subprocess.DEVNULL,
                 stderr=subprocess.PIPE if self.stream_output else subprocess.DEVNULL,
                 env=input_env,
+                cwd=self.working_dir,
                 **open_kwargs,
             )
         )
-        return ShellJobRun(shell_job=self, process=process)
+        return ShellProcess(shell_operation=self, process=process)
+
+    @sync_compatible
+    async def run(self, **open_kwargs: Dict[str, Any]) -> List[str]:
+        """
+        Triggers a shell operation, but unlike the trigger method,
+        additionally waits and fetches the result. This is a convenience
+        method for short-lasting shell operations.
+
+        Args:
+            **open_kwargs: Additional keyword arguments to pass to `open_process`
+
+        Returns:
+            The lines output from the shell operation as a list.
+        """
+        job_run = await self.trigger(**open_kwargs)
+        await job_run.wait_for_completion()
+        return await job_run.fetch_result()
 
     @sync_compatible
     async def close(self):
         """
         Close the job block.
         """
-        await self._exit_stack.close()
+        await self._exit_stack.aclose()
+        self.logger.info("Successfully closed all open processes.")
+
+    async def aclose(self):
+        """
+        Asynchronous version of the close method.
+        """
+        await self.close()
+
+    async def __aenter__(self) -> "ShellCommand":
+        """
+        Asynchronous version of the enter method.
+        """
+        return self
+
+    async def __aexit__(self, *exc_info):
+        """
+        Asynchronous version of the exit method.
+        """
+        await self.close()
+
+    def __enter__(self) -> "ShellCommand":
+        """
+        Enter the context of the job block.
+        """
+        return self
+
+    def __exit__(self, *exc_info):
+        """
+        Exit the context of the job block.
+        """
+        self.close()
